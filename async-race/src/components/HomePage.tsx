33e5e64b00e7ed1d8566b989856261db8d42a6f7
mishkamag <m93.maglaperidze@gmail.com>
2024-04-10 23:42:30 +0400

    feat: add create 100 cars functionality

--- async-race/src/components/HomePage.tsx
+++ async-race/src/components/HomePage.tsx
@@ -8,9 +8,20 @@ export interface CarInterface {
   color: string;
 }
 
+const carNames = {
+  Porshe: ["Panamera", "Cayene", "Boxter", "911"],
+  BMW: ["M3 e46", "M4", "540i", "X6"],
+  Mersedes: ["AMG G55", "GT63 AMG", "CLS"],
+  Lada: ["Priora", "Granta", "X-Ray", "2101"],
+  Volkswagen: ["Polo GT", "Scirocco", "Golf GTI", "Beetle"],
+  Hundai: ["Tiburon", "Santa Fe", "Accent", "Sonata"],
+  Lamborgini: ["Aventador", "Urus", "Huracan", "Veneno"],
+};
+
 const HomePage = () => {
   const [carsData, setCarsData] = useState<CarInterface[]>([]);
 
+  //get all cars
   const getCars = () => {
     fetch(`http://localhost:3000/garage`)
       .then((response) => {
@@ -27,6 +38,7 @@ const HomePage = () => {
       });
   };
 
+  //add car
   const addCar = (obj: { name: string; color: string }) => {
     fetch("http://localhost:3000/garage", {
       method: "POST",
@@ -35,6 +47,19 @@ const HomePage = () => {
     })
       .then<CarInterface>((response) => response.json())
       .then(() => {});
+  };
+
+  //create 100 cars random
+  const create100Cars = () => {
+    for (let i = 0; i < 100; i++) {
+      const arr = Object.entries(carNames);
+      const firstName = arr[Math.floor(Math.random() * arr.length)];
+      const secondName =
+        firstName[1][Math.floor(Math.random() * firstName[1].length)];
+      const name = `${firstName[0]} ${secondName}`;
+      const color = "#" + Math.random().toString(16).slice(3, 9);
+      addCar({ name, color });
+    }
   };
 
   useEffect(() => {
@@ -54,7 +79,9 @@ const HomePage = () => {
         <p className="garage-text">Cars in garage: {carsData?.length}</p>
         <button className="app-button">Start race</button>
         <button className="app-button">Reset</button>
-        <button className="app-button">Create 100 cars</button>
+        <button onClick={create100Cars} className="app-button">
+          Create 100 cars
+        </button>
       </div>
 
       <SingleCar carsData={carsData} />
